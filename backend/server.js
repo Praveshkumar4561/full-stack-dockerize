015a60ff7da905264041c746f8d21af9b93cc6b5
Pravesh Kumar <pravesh4561@gmail.com>
2025-11-09 18:58:33 +0530

    docker commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:3000",
+    ],
+  })
+);
+
+app.get("/api/message", (req, res) => {
+  res.json("message received successfully");
+});
+
+app.listen(1700, "0.0.0.0", () => {
+  console.log("server is running on port 1700");
+});
